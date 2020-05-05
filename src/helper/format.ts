5b74e935fd39bf9a2934c05814560782b08ceb3b
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2020-05-06 03:19:58 +0900

    fix, post list date format rule

--- src/helper/format.ts
+++ src/helper/format.ts
@@ -11,7 +11,7 @@ export const dateFormat = (now: Date, target: Date): string => {
     return `${minutes}分前`;
   }
 
-  if (diff < 60 * 60 * 60 * 1000) {
+  if (diff < 24 * 60 * 60 * 1000) {
     const hours = Math.floor(diff / (1000 * 60 * 60));
     return `${hours}時間前`;
   }
